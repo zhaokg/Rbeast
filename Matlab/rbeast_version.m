12fe758a9b1378ba026fec890b1eab49cd4e0ed4
zhaokg <lidar.rs@gmail.com>
2022-10-02 02:12:36 -0400

    very minor improvement

--- Matlab/rbeast_version.m
+++ Matlab/rbeast_version.m
@@ -1,3 +1,7 @@
-% The larger the number, the higher/latest version it is
 
-rbeastGitHubVersion = 0.9411;
+% Oct 2., 2022: fix a small bug "int ParseInputData( BEAST2_IO_PTR _OUT_ io)"
+% The pixel indices are 1-based not 0-based. This bug has no effects on the 
+% model results but just improve the robutness of the program
+
+% The larger the number, the higher/latest version it is
+rbeastGitHubVersion = 0.9412;
